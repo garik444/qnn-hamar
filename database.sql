1450efc3d3ea36632ffdd6244a4398a933f5364b
garik444 <gghevondyan21@gmail.com>
2026-06-16 10:42:20 +0400

    Create initial database schema and seed data

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (...);
+
+CREATE TABLE product (...);
+
+CREATE TABLE category (...);
+
+CREATE TABLE product_category (...);
+
+INSERT INTO users ...;
+INSERT INTO product ...;
+...
